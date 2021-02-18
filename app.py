2fd7ab7c02ad5771564f666110af99aaf7f866b2
Kei Fukutani <kifinewera@gmail.com>
2021-02-17 18:30:54 -0800

    setup database and migration

--- app.py
+++ app.py
@@ -2,15 +2,29 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import db, setup_db, Person
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+    @app.route('/')
+    def get_greeting():
+        greeting = "Hello"
 
-APP = create_app()
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
+
+
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-
+export AUTH0_DOMAIN=
+export Auth0_CLIENT=
+export API_AUDIENCE=
+export ALGORITHMS=
+export DATABASE_URL=
+export JWT_SECRET=
