a9ebdfd1191b13cf056649ae5c4f6b014af5c7bc
Kei Fukutani <kifinewera@gmail.com>
2021-02-17 22:24:28 -0800

    update

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-# database_path = os.environ['DATABASE_URL']
-database_name = "capstone"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
+# database_name = "capstone"
+# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
