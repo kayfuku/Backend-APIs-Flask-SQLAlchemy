8f8ed1f78cadb953868201b5a2ae86c4799d4613
Kei Fukutani <kifinewera@gmail.com>
2021-02-17 22:06:30 -0800

    add test env var

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ def create_app(test_config=None):
 
     @app.route('/')
     def get_greeting():
+        excited = os.environ['EXCITED']
         greeting = "Hello"
+        if excited == 'true':
+            greeting = greeting + "!!!!!"
 
         return greeting
 
